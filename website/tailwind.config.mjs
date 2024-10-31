6c999ad8518ea12dc00023981787e665554f09b2
ttttttomas <totobarajas124@gmail.com>
2024-10-31 10:52:01 -0300

    add aside and icons

--- website/tailwind.config.mjs
+++ website/tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bgAside: "rgba(255, 255, 255, 0.7)"
+
+			}
+		},
 	},
 	plugins: [],
 }
